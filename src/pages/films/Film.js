2db93c789f9b19175f87f955be4f0e22483d0126
Mathis Legrand <m.legrand91600@gmail.com>
2022-09-09 22:36:17 +0200

    getting out useless import

--- src/pages/films/Film.js
+++ src/pages/films/Film.js
@@ -2,12 +2,6 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getSingleFilm } from "../../services/generalService";
 import SingleCardMovie from "../../components/SingleCardMovie";
-import film1 from '../../images/films/film1.jpg'
-import film2 from '../../images/films/film2.jpg'
-import film3 from '../../images/films/film3.jpg'
-import film4 from '../../images/films/film4.jpg'
-import film5 from '../../images/films/film5.jpg'
-import film6 from '../../images/films/film6.jpg'
 import './Film.css'
 
 const Film = () => {
